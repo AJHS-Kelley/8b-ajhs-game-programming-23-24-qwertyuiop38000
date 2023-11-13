db3f084939e0bc82abb2be377fe1eae382e25e2f
qwertyuiop38000 <111384846+qwertyuiop38000@users.noreply.github.com>
2023-11-13 14:21:33 -0500

    everything

---
+++ 05a_exampleGameFunctions/exampleGameFunctions.py
@@ -0,0 +1,14 @@
+# Example Game Functions Projects, William Castengera, v0.1
+import random
+
+def functionOne():
+    pass
+
+def functionTwo(param1):
+    pass
+
+def functionThree(param1 = "default value"):
+    pass
+
+def functionfour(param1, param2, param3):
+    pass
