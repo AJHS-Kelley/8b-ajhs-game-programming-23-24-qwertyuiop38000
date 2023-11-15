d7aaf54ba2c3b40e180da07a9a4129ca154cea9b
qwertyuiop38000 <111384846+qwertyuiop38000@users.noreply.github.com>
2023-11-15 14:26:28 -0500

    v0.3

--- 05a_exampleGameFunctions/exampleGameFunctions.py
+++ 05a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,21 +1,34 @@
 # Example Game Functions Projects, William Castengera, v0.2
 import random
 
-character = input("Choose a character Ivan, Chad or Jim")
-
+character = str(input("Choose a character Ivan, Chad or Jim"))
+enemyList = ['hobgoblin', 'hellhound', 'cobalt', 'orc', 'troglodyte', ]
 def criticalHit():
     hit = random.randint(1, 20)
     if hit == 20:
         return True
     else:
         return False
-def damageDone(isCritical, character):
-    isCritical = criticalHit()
-    if isCritical == True and character == "Jim":
-        damage = random.randint()
+isCritical = criticalHit()
+def damageDone(isCrit, char):
+    if isCritical == True:
+        if character == "Chad":
+            damage = random.randint(250, 300)
+            return damage
+        elif character == "Ivan":
+            damage = random.randint(250, 300)
+            return damage
+        else:
+            damage = random.randint(200, 250)
+            return damage
+    else:
+        damage = random.randint(100, 150)
+        return damage
 
 def functionThree(param1 = "default value"):
     pass
 
 def functionfour(param1, param2, param3):
     pass
+damage = damageDone(isCritical, character)
+print(damage)
