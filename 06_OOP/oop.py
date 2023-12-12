527a22d9165aff59617d3cc9c0eefe93126b69b7
qwertyuiop38000 <111384846+qwertyuiop38000@users.noreply.github.com>
2023-12-12 13:52:25 -0500

    v0.1
    dskjdsjkdskjkd

--- 06_OOP/oop.py
+++ 06_OOP/oop.py
@@ -1,4 +1,15 @@
-# Object-Oriented Programming, William Castengera, v0.0
+# Object-Oriented Programming, William Castengera, v0.1
 
 class Person:# Use PascalCase for ClassNames
-    
+    def __init__(self, name, age, weight):
+        self.name = name
+        self.age = age
+        self.weight = weight
+
+        # To string function --- how the object appears as a string
+        def __str__(self):
+            return f"Name: {self.name}\nThis person is {self.age} years old.\nThey weigh {self.weight} pounds.\n"
+
+
+person1 = Person("John D. Sugma", 74, 350)
+print(person1)
