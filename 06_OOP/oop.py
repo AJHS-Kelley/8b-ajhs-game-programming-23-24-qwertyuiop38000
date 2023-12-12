6bc650ade393f6496314a376a9b3f6f076c1ff79
qwertyuiop38000 <111384846+qwertyuiop38000@users.noreply.github.com>
2023-12-12 14:24:39 -0500

    v0.4

--- 06_OOP/oop.py
+++ 06_OOP/oop.py
@@ -10,7 +10,7 @@ class Person:# Use PascalCase for ClassNames
     def __str__(self):
         return f"Name: {self.name}\nThis person is {self.age} years old.\nThey weigh {self.weight} pounds.\n"
 
-    def classFunction():
+    def classFunction(self):
         print("this is an example class function\n")
         print("It only works when called on an object of that class")
 
@@ -18,19 +18,33 @@ class Person:# Use PascalCase for ClassNames
 
 person1 = Person("John D. Sugma", 74, 350)
 person2 = Person("John D. ligma", 76, 360)
+# print(person1)
+# print(person2)
+
+# if person1.weight > person2.weight:
+#     print(f"{person1.name} weighs more than {person2.name}\n")
+# elif person1.weight == person2.weight:
+#     print("Each person weighs the same.\n")
+# else: 
+#     print(f"{person2.name} weighs more than {person1.name}\n")
+
+# if person1.age > person2.age:
+#     print(f"{person1.name} is older than {person2.name}\n")
+# elif person1.weight == person2.weight:
+#     print("Each person is the same age.\n")
+# else: 
+#     print(f"{person2.name} is older than {person1.name}\n")
+
+
+# person1.classFunction()
+
+# Changes properties after creation
+print(person1.name)
+person1.name = "Jack the ripper"
+print(person1.name)
+
+# Deleting Properties
+
+print(person1.name)
+del person1.name
 print(person1)
-print(person2)
-
-if person1.weight > person2.weight:
-    print(f"{person1.name} weighs more than {person2.name}\n")
-elif person1.weight == person2.weight:
-    print("Each person weighs the same.\n")
-else: 
-    print(f"{person2.name} weighs more than {person1.name}\n")
-
-if person1.age > person2.age:
-    print(f"{person1.name} is older than {person2.name}\n")
-elif person1.weight == person2.weight:
-    print("Each person is the same age.\n")
-else: 
-    print(f"{person2.name} is older than {person1.name}\n")
