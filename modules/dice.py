34f042bd0d1b8d3548f347a6d90b4378c1678a14
qwertyuiop38000 <111384846+qwertyuiop38000@users.noreply.github.com>
2023-12-14 13:41:02 -0500

    v0.02

--- modules/dice.py
+++ modules/dice.py
@@ -1,7 +1,16 @@
 # Dice Roling Module by Wiliam Castengera, v0.0
 import random
 
-def rollDice(numDice, sizeDice):
+def roll(numDice, sizeDice):
+    numRolled = 0
+    sum = 0
+    while numRolled < numDice:
+        roll = random.randint(1, sizeDice)
+        numRolled += 1
+        sum += roll
+    return sum 
+
+def display(numDice, sizeDice):
     numRolled = 0
     sum = 0
     while numRolled < numDice:
@@ -10,4 +19,5 @@ def rollDice(numDice, sizeDice):
         numRolled += 1
         sum += roll
         print(f"Sum: {sum}\n")
-    return sum 
+    return sum
+
