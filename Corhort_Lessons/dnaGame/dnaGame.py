3eb328b02c1ff15b9f54d1e33045049a49c41ec9
qwertyuiop38000 <111384846+qwertyuiop38000@users.noreply.github.com>
2024-01-11 14:24:00 -0500

    v0.2

--- Corhort_Lessons/dnaGame/dnaGame.py
+++ Corhort_Lessons/dnaGame/dnaGame.py
@@ -1,7 +1,27 @@
-# DNA Replication Game, William Castengera, v0.1
+# DNA Replication Game, William Castengera, v0.2
 
 # Import Entire Modules -- Get the whole tool box.
 import  time, datetime 
 
 # Importing Specific Methods -- Get the specific tool
 from random import choice
+
+# Store the DNA Bases
+dnaBases = ["A", "T", "G", "C"]
+
+# GAME FUNCTIONS
+def gameIntro() -> None:
+    pass
+
+def genDna() -> str:
+    basesGenerated = 0
+    basesRequested = int(input("Please enter a positive integer number of bases to generate\n"))
+    dnaSequence = ""
+    while basesGenerated < basesRequested:
+        dnaSequence += choice(dnaBases)
+        basesGenerated += 1
+    return dnaSequence
+
+dna = genDna()
+print(dna)
+
