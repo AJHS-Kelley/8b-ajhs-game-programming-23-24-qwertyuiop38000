f4e63a06780ab66a66cfea2049e64c8484f7e87e
qwertyuiop38000 <111384846+qwertyuiop38000@users.noreply.github.com>
2024-02-01 14:07:12 -0500

    465768

--- 08_ultimateIntroPygame/Game.py
+++ 08_ultimateIntroPygame/Game.py
@@ -1,8 +1,13 @@
 # William Castengera, 2/1/24, v0.0
 import pygame
+from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 
 while True:
-    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    pygame.display.update()
