8416dc93d06931db5f3f8827fb4880e99907b3a4
qwertyuiop38000 <111384846+qwertyuiop38000@users.noreply.github.com>
2024-02-01 14:35:44 -0500

    345678

--- 08_ultimateIntroPygame/Game.py
+++ 08_ultimateIntroPygame/Game.py
@@ -7,10 +7,15 @@ screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 
+test_surface = pygame.image.load('img/Downloads/OneDrive - Duval County Public Schools/8b-ajhs-game-programming-23-24-qwertyuiop38000/08_ultimateIntroPygame/Sky_Image.png')
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    screen.blit(test_surface, (0,0))
+
     pygame.display.update()
     clock.tick(60)
