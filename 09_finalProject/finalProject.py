2dc96289c8922fc9a554cd019b07bd4a31d5d681
qwertyuiop38000 <111384846+qwertyuiop38000@users.noreply.github.com>
2024-04-17 14:52:49 -0400

    7467875346753478564

--- 09_finalProject/finalProject.py
+++ 09_finalProject/finalProject.py
@@ -17,32 +17,28 @@ else:
     x = 1920
     y = 1080
 screen = pygame.display.set_mode((x, y))
-pos = (0,0)
+z = 680
+c = 550
 Maze_Surface = pygame.image.load('img/imagesX_800x600.png').convert_alpha()
 character_surface = pygame.image.load('img/character.png').convert_alpha()
-character = character_surface.get_rect(topleft = (680,550))
-def getCoords(self):
-    return [self.pos[0], self.pos[1], self.pos[0] + self.size[0], self.pos[1] + self.size[1]]
-def updatePos(self, coords):
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP] and self.getCoords()[1]> self.minPos[1]:
-        self.pos[1] -= 0.3
-    if keys[pygame.K_DOWN] and self.getCoords()[3]< self.maxPos[1]:
-        self.pos[1] += 0.3
-    if keys[pygame.K_w] and self.getCoords()[1]> self.minPos[1]:
-        self.pos[1] -= 0.3
-    if keys[pygame.K_s] and self.getCoords()[3]< self.maxPos[1]:
-        self.pos[1] += 0.3
-def __init__(self):
-    self.pos = pos
+
+
 while True:
+    character = character_surface.get_rect(topleft = (z,c))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
     screen.blit(Maze_Surface, (0,0))
     screen.blit(character_surface, character)
-    __init__(character)
-    updatePos(character, getCoords(character))
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        c -= 5
+    if keys[pygame.K_a]:
+        z -= 5
+    if keys[pygame.K_d]:
+        z += 5
+    if keys[pygame.K_s]:
+        c += 5
     pygame.display.update()
     clock.tick(60)
