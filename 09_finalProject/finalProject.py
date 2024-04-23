7ff3db7c0ed8506a7a06608c2f6c97897068a6cf
qwertyuiop38000 <111384846+qwertyuiop38000@users.noreply.github.com>
2024-04-23 13:47:00 -0400

    redtfgujmkijuhtfrdfhujko;l

--- 09_finalProject/finalProject.py
+++ 09_finalProject/finalProject.py
@@ -21,24 +21,48 @@ z = 680
 c = 550
 Maze_Surface = pygame.image.load('img/imagesX_800x600.png').convert_alpha()
 character_surface = pygame.image.load('img/character.png').convert_alpha()
+test_surface = pygame.image.load('img/41509588-16212fda-721c-11e8-8650-dc1cdd619072.png').convert_alpha()
 
 
 while True:
     character = character_surface.get_rect(topleft = (z,c))
+    test_rect = test_surface.get_rect(topleft = (100,100))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
     screen.blit(Maze_Surface, (0,0))
     screen.blit(character_surface, character)
+    screen.blit(test_surface, test_rect)
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        c -= 5
-    if keys[pygame.K_a]:
-        z -= 5
-    if keys[pygame.K_d]:
+    if keys[pygame.K_w] or keys[pygame.K_UP]:
+        if (keys[pygame.K_w] or keys[pygame.K_UP]) and (keys[pygame.K_a] or keys[pygame.K_LEFT]):
+            if character.colliderect(test_rect):
+                z += 5
+                continue
+            else:
+                c -= 5
+                z -= 5
+        elif (keys[pygame.K_w] or keys[pygame.K_UP]) and (keys[pygame.K_d] or keys[pygame.K_RIGHT]):
+            if character.colliderect(test_rect):
+                z -= 5
+                continue
+            else:
+                c -= 5
+                z += 5
+        elif character.colliderect(test_rect):
+            c += 5
+        else:
+            c -= 5
+    if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+        if character.colliderect(test_rect):
+            z += 5
+            continue
+        else:
+            z -= 5
+    if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
         z += 5
-    if keys[pygame.K_s]:
+    if keys[pygame.K_s] or keys[pygame.K_DOWN]:
         c += 5
     pygame.display.update()
     clock.tick(60)
