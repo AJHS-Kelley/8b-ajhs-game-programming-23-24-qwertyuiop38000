4e1d99d26118ac24c9764cb0085fb00ea49b9ddc
qwertyuiop38000 <111384846+qwertyuiop38000@users.noreply.github.com>
2024-04-23 13:52:13 -0400

    srgjbkgjkerjgkeokdnjrfkgkpdklgkjlkngfkljkjkjfkjgfkj

--- tryExceptFinally/tryExceptFinally.py
+++ tryExceptFinally/tryExceptFinally.py
@@ -4,8 +4,12 @@
 try: # the code in this block is ALWAYS executed.
     myVariable = 1
     print(myVariable)
+    myString = "five"
+    print(float(myString))
 except NameError: # this code will run IF there is an error (exception) raised.
     print("let there be name error")
+except ValueError:
+    print("you got value error")
 except:
     print("i don't know what happened you're on your own")
 else:
